4a60b6a10e94d692d6b40b31de91bc10835c8b12
trevorpreston <trevorpreston4@gmail.com>
2019-06-03 18:33:40 -0400

    add bot selection options to bot config form

--- src/components/botConfig/Configuration.js
+++ src/components/botConfig/Configuration.js
@@ -5,17 +5,22 @@ import ManualConfigForm from './ManualConfigForm'
 class Configuration extends Component {
   constructor() {
     super();
-    this.state = { 
+    this.state = {
       manual: false,
       wateringPeriod: 0,
-      pastPlants: []
+      pastPlants: [],
+      bots: [],
+      selectedBot: ''
     }
   }
 
   componentDidMount() {
     // fetchPastPlants();  
-    this.setState({  // use dummy data until API is ready.
-      pastPlants: [{plant1: 7}, {plant2: 3}, {plant3: 11}, {plant4: 9}, {plant5: 4}]
+    // fetchBots();
+    /* NOTE: using dummy data until API is complete. */
+    this.setState({  
+      pastPlants: [{orchid: 7}, {sunflower: 8}, {bogenzelia: 11}, {snapdragon: 4}, {succulent: 12}],
+      bots: [{id: 1, name: 'office', connection: 'url1'}, {id: 2,  name: 'kitchen', connection: 'url2'}, {id: 3, name: 'living room', connection: 'url3'}]
     })
   }
 
@@ -31,13 +36,45 @@ class Configuration extends Component {
       })
   }
 
-  renderForm() {
-    return this.state.manual ? <ManualConfigForm/> : <AutoConfigForm/>
+  fetchBots() {
+    fetch('localhost:3000/botregistry')
+    .then(results => {
+      results.json();
+    })
+    .then(data => {
+      this.setState({
+        bots: data
+      });
+    })
+  }
+
+  updateBot() {
+    this.setState({selectedBot: 'booger'})
   }
 
   toggleFormType() {
     this.setState({manual: !this.state.manual})
   }
+
+  renderBotOptions() {
+    return this.state.bots.map(bot => {
+      return <option value={bot.name} key={bot.id}>{bot.name}</option>
+    })
+  }
+
+  renderForm() {
+    return (
+      <div>
+        <select name="bot-select" id="" onChange={this.updateBot}>
+          {this.renderBotOptions()}
+        </select>
+        {this.state.manual ? <ManualConfigForm/> : <AutoConfigForm/>}
+        <button onClick={this.updateBot}></button>
+      </div>
+    )
+  }
+
+
 
   render() {
     return (
